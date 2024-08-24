ef9554a513ce79534d1b66cd8ae35c78f433fc4a
Mel <melbraeisantiago@gmail.com>
2024-08-24 15:05:34 +0800

    v.1.1

--- dashboard/src/pages/Livestock and Poultry DRRM/DiseaseIncidenceReport.js
+++ dashboard/src/pages/Livestock and Poultry DRRM/DiseaseIncidenceReport.js
@@ -10,22 +10,28 @@ import {
   Box,
   Typography,
   TextField,
-  Button
+  Button,
 } from "@mui/material";
 import "./DiseaseIncidenceReport.css"; // Ensure this file exists and is styled correctly
 
 const DiseaseIncidenceReport = () => {
   const [name, setName] = useState("");
+  const [position, setPosition] = useState("");
   const [date, setDate] = useState("");
+  const [submitted, setSubmitted] = useState("");
+  const [checked, setChecked] = useState("");
+  const [noted, setNoted] = useState("");
 
   const handleNameChange = (event) => setName(event.target.value);
   const handleDateChange = (event) => setDate(event.target.value);
+  const handlePositionChange = (event) => setPosition(event.target.value);
+  const handleSubmitted = (event) => setSubmitted(event.target.value);
+  const handleNoted = (event) => setNoted(event.target.value);
+  const handleChecked = (event) => setChecked(event.target.value);
 
   return (
     <>
-      <div className="title">
-        Disease Surveillance and Incidence Report
-      </div>
+      <div className="title">Disease Surveillance and Incidence Report</div>
       <TableContainer component={Paper} className="table-container">
         <Table
           size="small"
@@ -74,42 +80,54 @@ const DiseaseIncidenceReport = () => {
           <TableBody>
             {/* Animal Categories and Diseases */}
             {[
-              { category: "Avian Poultry", diseases: [
-                "Avian Influenza",
-                "Newcastle Disease (ND)"
-              ]},
-              { category: "Swine", diseases: [
-                "Foot and Mouth Disease",
-                "Classical Swine Fever"
-              ]},
-              { category: "Cattle", diseases: [
-                "Blackleg",
-                "Anthrax",
-                "Foot and Mouth Disease (FMD)",
-                "Hemorrhagic Septicemia",
-                "Surra"
-              ]},
-              { category: "Carabao", diseases: [
-                "Anthrax",
-                "Foot and Mouth Disease (FMD)",
-                "Hemorrhagic Septicemia",
-                "Surra"
-              ]},
-              { category: "Goat", diseases: [
-                "Rabies",
-                "Anthrax",
-                "Foot and Mouth Disease (FMD)",
-                "Hemorrhagic Septicemia",
-                "Caprine Arthritis Encephalitis (CAE)"
-              ]},
-              { category: "Sheep", diseases: [
-                "Anthrax",
-                "Foot and Mouth Disease (FMD)",
-                "Hemorrhagic Septicemia",
-                "Caprine Arthritis Encephalitis (CAE)"
-              ]},
-              { category: "Dog", diseases: ["Rabies"]},
-              { category: "Cat", diseases: ["Rabies"]}
+              {
+                category: "Avian Poultry",
+                diseases: ["Avian Influenza", "Newcastle Disease (ND)"],
+              },
+              {
+                category: "Swine",
+                diseases: ["Foot and Mouth Disease", "Classical Swine Fever"],
+              },
+              {
+                category: "Cattle",
+                diseases: [
+                  "Blackleg",
+                  "Anthrax",
+                  "Foot and Mouth Disease (FMD)",
+                  "Hemorrhagic Septicemia",
+                  "Surra",
+                ],
+              },
+              {
+                category: "Carabao",
+                diseases: [
+                  "Anthrax",
+                  "Foot and Mouth Disease (FMD)",
+                  "Hemorrhagic Septicemia",
+                  "Surra",
+                ],
+              },
+              {
+                category: "Goat",
+                diseases: [
+                  "Rabies",
+                  "Anthrax",
+                  "Foot and Mouth Disease (FMD)",
+                  "Hemorrhagic Septicemia",
+                  "Caprine Arthritis Encephalitis (CAE)",
+                ],
+              },
+              {
+                category: "Sheep",
+                diseases: [
+                  "Anthrax",
+                  "Foot and Mouth Disease (FMD)",
+                  "Hemorrhagic Septicemia",
+                  "Caprine Arthritis Encephalitis (CAE)",
+                ],
+              },
+              { category: "Dog", diseases: ["Rabies"] },
+              { category: "Cat", diseases: ["Rabies"] },
             ].map((item, index) => (
               <React.Fragment key={index}>
                 <TableRow>
@@ -129,7 +147,11 @@ const DiseaseIncidenceReport = () => {
                     </TableCell>
                     {/* Empty Cells for Number of Cases and Intervention */}
                     {[...Array(8)].map((_, j) => (
-                      <TableCell key={j} className="table-cell-body" align="center"></TableCell>
+                      <TableCell
+                        key={j}
+                        className="table-cell-body"
+                        align="center"
+                      ></TableCell>
                     ))}
                   </TableRow>
                 ))}
@@ -140,30 +162,97 @@ const DiseaseIncidenceReport = () => {
       </TableContainer>
 
       <Box mt={4} p={2} borderTop="1px solid #ccc">
-        <Typography variant="body1">Prepared by:</Typography>
-        <TextField
-          value={name}
-          onChange={handleNameChange}
-          placeholder="Enter your name"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-        />
-        <Typography variant="body1">Date:</Typography>
-        <TextField
-          value={date}
-          onChange={handleDateChange}
-          placeholder="Enter the date"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          type="date"
-          InputLabelProps={{ shrink: true }}
-        />
-        <Button variant="contained" color="primary" style={{ marginTop: 16 }}>
-          Save
-        </Button>
-      </Box>
+  {/* Date Field */}
+  <Typography variant="body1" sx={{ fontSize: "12px" }}>
+    Date:
+  </Typography>
+  <TextField
+    value={date}
+    onChange={handleDateChange}
+    placeholder="Enter the date"
+    variant="outlined"
+    margin="none"
+    type="date"
+    InputLabelProps={{ shrink: true }}
+  />
+
+  {/* Fields in a Row */}
+  <Box display="flex" flexDirection="row" gap={2} mt={2}>
+    {/* Prepared by */}
+    <Box flex="1">
+      <Typography variant="body1" sx={{ fontSize: "12px" }}>
+        Prepared by:
+      </Typography>
+      <TextField
+        value={name}
+        onChange={handleNameChange}
+        placeholder="Name"
+        variant="outlined"
+        margin="none"
+        size="small"
+        sx={{ fontSize: "20px" }} // Larger font size for the name
+        fullWidth
+      />
+    </Box>
+
+    {/* Submitted by */}
+    <Box flex="1">
+      <Typography variant="body1" sx={{ fontSize: "12px" }}>
+        Submitted by:
+      </Typography>
+      <TextField
+        value={submitted}
+        onChange={handleSubmitted}
+        placeholder="Name"
+        variant="outlined"
+        margin="none"
+        size="small"
+        sx={{ fontSize: "14px" }} // Adjust size as needed
+        fullWidth
+      />
+    </Box>
+
+    {/* Checked by */}
+    <Box flex="1">
+      <Typography variant="body1" sx={{ fontSize: "12px" }}>
+        Checked by:
+      </Typography>
+      <TextField
+        value={checked}
+        onChange={handleChecked}
+        placeholder="Name"
+        variant="outlined"
+        margin="none"
+        size="small"
+        sx={{ fontSize: "14px" }} // Adjust size as needed
+        fullWidth
+      />
+    </Box>
+
+    {/* Noted by */}
+    <Box flex="1">
+      <Typography variant="body1" sx={{ fontSize: "12px" }}>
+        Noted by:
+      </Typography>
+      <TextField
+        value={noted}
+        onChange={handleNoted}
+        placeholder="Name"
+        variant="outlined"
+        margin="none"
+        size="small"
+        sx={{ fontSize: "14px" }} // Adjust size as needed
+        fullWidth
+      />
+    </Box>
+  </Box>
+
+  {/* Save Button */}
+  <Button variant="contained" color="primary" style={{ marginTop: 50 }}>
+    Save
+  </Button>
+</Box>
+
     </>
   );
 };
