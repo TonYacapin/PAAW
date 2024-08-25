83059f2ec2a11ef0f805531baf9de5a45f9b43b3
Abakus <yacapinton@gmail.com>
2024-08-25 11:58:33 +0800

    tite

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -3,21 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-      'darkgreen': '#1b5b40',
-      'white': '#fffafa',
-      'nero': '#252525',
-      'pastelyellow': '#ffe459',
-    },
+    // colors: {
+    //   'darkgreen': '#1b5b40',
+    //   'white': '#fffafa',
+    //   'nero': '#252525',
+    //   'pastelyellow': '#ffe459'
+    // },
     extend: {},
   },
   plugins: [],
