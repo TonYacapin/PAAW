3afad857fb861966431d1dc247e9b390fa24ccf9
Trent <65882951+Dre-OS@users.noreply.github.com>
2024-08-25 11:22:50 +0800

    added home route

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import DiseaseIncidenceReport from './pages/Livestock and Poultry DRRM/DiseaseIncidenceReport';
 import RabiesVaccinationReport from './pages/RABIES/RabiesVaccinationReport';
-
+import Home from "./pages/Home/Home";
 const App = () => {
   return (
     <Router>
@@ -12,6 +12,7 @@ const App = () => {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/dif" element={<DiseaseIncidenceReport />} />
           <Route path="/Rabies" element={<RabiesVaccinationReport />} />
+          <Route path="/Home" element={<Home />} />
           {/* Add more routes here if needed */}
         </Routes>
       </div>
