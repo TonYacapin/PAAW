504ac61b555ddcfc37f467d1a0f14b8395167852
Trent <65882951+Dre-OS@users.noreply.github.com>
2024-09-02 10:46:47 +0800

    modified navbar component

--- dashboard/src/component/Navbar.js
+++ dashboard/src/component/Navbar.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="h-14 w-full bg-darkgreen">
-
+      <div className="h-3/6">
+        <img src="./pages/assets/PAAW.png" alt="Home" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
