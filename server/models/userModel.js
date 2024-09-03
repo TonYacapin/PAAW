d682384596086ee2f05e812942338a679106caab
Abakus <yacapinton@gmail.com>
2024-09-03 14:05:37 +0800

    updated the user roles in backend

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['user', 'admin', 'superadmin'], // Define roles as needed
+    enum: ['user', 'admin', 'regulatory', 'animalhealth', 'livestock'], // Define roles as needed
     default: 'user',
   },
   isActive: {
