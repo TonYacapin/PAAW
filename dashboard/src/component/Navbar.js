43ec318f79f8de8aecbc0b86f362cfea0956e8f7
Abakus <yacapinton@gmail.com>
2024-09-03 18:03:19 +0800

    Updated Dashboard

--- dashboard/src/component/Navbar.js
+++ dashboard/src/component/Navbar.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from '../pages/assets/PAAW.png';
 
 function Navbar() {
   const role = localStorage.getItem('userRole');
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const handleLogout = () => {
     localStorage.clear();
@@ -12,59 +14,91 @@ function Navbar() {
   };
 
   const renderButtons = () => {
+    const baseClasses = "px-4 py-2 rounded-md text-white hover:bg-[#174932] transition-colors";
+    
     switch (role) {
       case 'admin':
         return (
           <>
-            <button className="text-white">Clients</button>
-            <button className="text-white">Admin</button>
-            <button className="text-white">Animal Health</button>
-            <button className="text-white">Livestock</button>
-            <button className="text-white">Regulatory</button>
-            <button className="text-white" onClick={handleLogout}>Logout</button>
+            <button className={baseClasses}>Clients</button>
+            <button className={baseClasses}>Admin</button>
+            <button className={baseClasses}>Animal Health</button>
+            <button className={baseClasses}>Livestock</button>
+            <button className={baseClasses}>Regulatory</button>
+            <button className={`${baseClasses} bg-red-600`} onClick={handleLogout}>Logout</button>
           </>
         );
       case 'regulatory':
         return (
           <>
-            <button className="text-white">Clients</button>
-            <button className="text-white">Regulatory</button>
-            <button className="text-white" onClick={handleLogout}>Logout</button>
+            <button className={baseClasses}>Clients</button>
+            <button className={baseClasses}>Regulatory</button>
+            <button className={`${baseClasses} bg-red-600`} onClick={handleLogout}>Logout</button>
           </>
         );
       case 'animalhealth':
         return (
           <>
-           <button className="text-white">Clients</button>
-            <button className="text-white">Animal Health</button>
-            <button className="text-white" onClick={handleLogout}>Logout</button>
+            <button className={baseClasses}>Clients</button>
+            <button className={baseClasses}>Animal Health</button>
+            <button className={`${baseClasses} bg-red-600`} onClick={handleLogout}>Logout</button>
           </>
         );
       case 'livestock':
         return (
           <>
-           <button className="text-white">Clients</button>
-            <button className="text-white">Livestock</button>
-            <button className="text-white" onClick={handleLogout}>Logout</button>
+            <button className={baseClasses}>Clients</button>
+            <button className={baseClasses}>Livestock</button>
+            <button className={`${baseClasses} bg-red-600`} onClick={handleLogout}>Logout</button>
           </>
         );
       case 'user':
       default:
         return (
           <>
-          
-            <button className="text-white">Clients</button>
-            <button className="text-white" onClick={handleLogout}>Logout</button>
+            <button className={baseClasses}>Clients</button>
+            <button className={`${baseClasses} bg-red-600`} onClick={handleLogout}>Logout</button>
           </>
         );
     }
   };
 
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
   return (
-    <div className="h-16 w-full bg-darkgreen flex items-center justify-between px-4">
-      <img src={Logo} alt="Home" className="h-12" />
-      <div className="flex-1 flex justify-center">
+    <div className="h-16 w-full bg-[#1b5b40] flex items-center justify-between px-6 py-2 shadow-lg">
+      {/* Logo */}
+      <img src={Logo} alt="Logo" className="h-12" />
+
+      {/* Desktop Menu */}
+      <div className="hidden md:flex flex-1 justify-center">
         <div className="flex space-x-4 bg-gray-800 p-2 rounded-md border border-gray-700">
+          {renderButtons()}
+        </div>
+      </div>
+
+      {/* Mobile Menu Button */}
+      <div className="md:hidden flex items-center">
+        <button 
+          onClick={() => setIsMenuOpen(!isMenuOpen)} 
+          className="text-white text-2xl"
+        >
+          {isMenuOpen ? '✖️' : '☰'}
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      <div ref={menuRef} className={`md:hidden fixed top-0 right-0 w-64 bg-[#1b5b40] h-full p-4 transition-transform duration-300 ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+        <div className="flex flex-col space-y-4">
           {renderButtons()}
         </div>
       </div>
--- dashboard/src/pages/Home/Home.js
+++ dashboard/src/pages/Home/Home.js
@@ -3,11 +3,21 @@ import Navbar from "../../component/Navbar";
 
 function Home() {
   const [userRole, setUserRole] = useState("");
+  const [selectedDivision, setSelectedDivision] = useState(null); // New state for the selected division
 
   useEffect(() => {
     const role = localStorage.getItem("userRole");
+    console.log("userRole:", role); // Debugging line
     setUserRole(role);
   }, []);
+
+  useEffect(() => {
+
+    if (userRole) {
+      setSelectedDivision(userRole);
+    }
+
+  }, [userRole]);
 
   const renderCharts = () => {
     switch (userRole) {
@@ -57,6 +67,135 @@ function Home() {
     }
   };
 
+  const renderForms = () => {
+    switch (selectedDivision) {
+      case "user":
+        return (
+          <>
+            <h3 className="text-xl font-semibold text-[#252525]">
+              Browse Forms from Client Forms
+            </h3>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Requisition Form
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Animal Production Services Request Form
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Veterinary Information Services Request Form
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Animal Health Care Services Request Form
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Regulatory Care Services Request Form
+            </button>
+          </>
+        );
+      case "admin":
+        return (
+          <>
+
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Clients
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Admin
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Animal Health
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Livestock
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Regulatory
+            </button>
+          </>
+        );
+      case "animalhealth":
+        return (
+          <>
+            <h3 className="text-xl font-semibold text-[#252525]">
+              Browse Forms from AnimalHealth Forms
+            </h3>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Requisition Form
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Rabies Vaccination
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Vaccination Report
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Generate Accomplishment Report
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Routine Service Monitoring Reports
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Disease Investigation Form
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Disease Surveillance and Incident Report
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Rabies History
+            </button>
+          </>
+        );
+      case "livestock":
+        return (
+          <>
+            <h3 className="text-xl font-semibold text-[#252525]">
+              Browse Forms from Livestock Forms
+            </h3>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Requisition Form
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Artificial Insemination
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Offspring Monitoring
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Estrus Synchronization
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Generate Monthly Accomplishment Reports
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Vitamin ADE Supplement
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Pregnancy Diagnostics
+            </button>
+          </>
+        );
+      case "regulatory":
+        return (
+          <>
+            <h3 className="text-xl font-semibold text-[#252525]">
+              Browse Forms from Regulatory Forms
+            </h3>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Requisition Form
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Veterinary Shipment
+            </button>
+            <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+              Manage Slaughter Shipment
+            </button>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -68,121 +207,61 @@ function Home() {
 
         {/* Right Side - Buttons */}
         <div className="mt-8 md:mt-0 md:w-1/3 space-y-6">
-          <h2 className="text-3xl font-bold text-[#252525] mb-6">
-            {userRole === "admin" ? "Browse Divisions" : "Browse Forms"}
-          </h2>
+          {/* Only show this for admin */}
+          {userRole === "admin" && (
+            <h2 className="text-3xl font-bold text-[#252525] mb-6">
+              Browse Divisions
+            </h2>
+          )}
+
           <div className="flex flex-col space-y-4">
             {userRole === "admin" && (
               <>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+                <button
+                  className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all"
+                  onClick={() => setSelectedDivision("user")}
+                >
                   Clients
                 </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+                <button
+                  className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all"
+                  onClick={() => setSelectedDivision("admin")}
+                >
                   Admin
                 </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+                <button
+                  className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all"
+                  onClick={() => setSelectedDivision("animalhealth")}
+                >
                   Animal Health
                 </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+                <button
+                  className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all"
+                  onClick={() => setSelectedDivision("livestock")}
+                >
                   Livestock
                 </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
+                <button
+                  className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all"
+                  onClick={() => setSelectedDivision("regulatory")}
+                >
                   Regulatory
                 </button>
               </>
             )}
-            {userRole === "user" && (
-              <>
-                 <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Requisition Form
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Animal Production Services Request Form
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Veterinary Information Services Request Form
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Animal Health Care Services Request Form
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Regulatory Care Services Request Form
-                </button>
-              </>
-            )}
-            {userRole === "regulatory" && (
-              <>
-                 <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Requisition Form
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Veterinary Shipment
-                </button>
 
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Slaughter Shipment
-                </button>
+            {/* Separator */}
+            <hr className="border-t-2 border-gray-300 my-4" />
 
-              
-                
-              </>
-            )}
-            {userRole === "animalhealth" && (
-              <>
-                 <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Requisition Form
-                </button>
-               <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Rabies Vacination
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Vacination Report
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                 Generate Accomplishment Report
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                 Routine Service Monitoring Reports
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                 Disease Investigation Form
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                 Disease Surveilance and Incident Report
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                 Manage Rabies History
-                </button>
-              
-              
-              
-              </>
-            )}
-            {userRole === "livestock" && (
-              <>
-                 <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Requisition Form
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Artificial Insemination
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Offpring Monitoring
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Estrus syncronization
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Generate Monthly Accomplishment Reports
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Vitamin ADE Supplement
-                </button>
-                <button className="bg-[#1b5b40] text-white py-3 px-4 rounded-md shadow-md hover:bg-[#174932] transition-all">
-                  Manage Pregnancy Diagnostics
-                </button>
-              </>
-            )}
+            {/* Display Division Name */}
+            {/* {selectedDivision && userRole === "admin" && (
+              <h3 className="text-xl font-semibold text-[#252525]">
+                Browse Forms from {selectedDivision.charAt(0).toUpperCase() + selectedDivision.slice(1)} Forms
+              </h3>
+            )} */}
+
+            {/* Render forms based on the selected division */}
+            {renderForms()}
           </div>
         </div>
       </div>
