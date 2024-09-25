c15470d9aa6ab4c8e3b5d45920282e3e54944a9f
Trent <65882951+Dre-OS@users.noreply.github.com>
2024-09-26 07:11:09 +0800

    added labels to edit modals

--- dashboard/src/pages/Animal Disease Prevention Control and Eradication/VaccinationReport.js
+++ dashboard/src/pages/Animal Disease Prevention Control and Eradication/VaccinationReport.js
@@ -414,14 +414,14 @@ function VaccinationReport() {
             <button
               type="button"
               onClick={() => openModal(index)}
-              className="px-4 py-2 bg-yellow-500 text-white rounded mr-2"
+              className="px-4 py-2 bg-darkgreen hover:bg-darkergreen text-white rounded mr-2"
             >
               Edit
             </button>
             <button
               type="button"
               onClick={() => openConfirmationModal(index)}
-              className="px-4 py-2 bg-red-500 text-white rounded"
+              className="px-4 py-2 bg-red-500 hover:bg-red-700 text-white rounded"
             >
               Remove
             </button>
@@ -458,7 +458,7 @@ function VaccinationReport() {
       {selectedEntry !== null && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
           <div className="bg-white p-8 rounded shadow-lg w-full max-w-3xl max-h-screen overflow-y-auto">
-            <h3 className="text-2xl font-bold mb-4">Edit Entry {entries[selectedEntry].no}</h3>
+            <h3 className="text-2xl font-bold mb-4">Edit Vaccination Entry {entries[selectedEntry].no}</h3>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
               <div>
@@ -625,14 +625,14 @@ function VaccinationReport() {
               <button
                 type="button"
                 onClick={closeModal}
-                className="px-4 py-2 bg-gray-500 text-white rounded mr-2"
+                className="px-4 py-2 bg-red-500 hover:bg-red-700 text-white rounded mr-2"
               >
                 Close
               </button>
               <button
                 type="button"
                 onClick={closeModal}
-                className="px-4 py-2 bg-darkgreen text-white rounded"
+                className="px-4 py-2 bg-darkgreen hover:bg-darkergreen text-white rounded"
               >
                 Save Changes
               </button>
