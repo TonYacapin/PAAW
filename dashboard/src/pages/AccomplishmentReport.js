afa55b8aa83f360495fde557efed171126355154
Mel <melbraeisantiago@gmail.com>
2024-10-01 17:45:12 +0800

    s

--- dashboard/src/pages/AccomplishmentReport.js
+++ dashboard/src/pages/AccomplishmentReport.js
@@ -144,7 +144,7 @@ function AccomplishmentReport() {
   };
 
   return (
-    <div className="p-6 bg-[#FFFAFA] max-h-[55vh] overflow-y-scroll">
+    <div className="p-6 bg-[#FFFAFA] max-h-[55vh]">
       <h1 className="text-3xl font-extrabold mb-6 text-[#1b5b40]">Accomplishment Report</h1>
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
 
--- dashboard/src/pages/RabiesVaccinationAccomplishmentReport.js
+++ dashboard/src/pages/RabiesVaccinationAccomplishmentReport.js
@@ -79,7 +79,7 @@ function RabiesVaccinationAccomplishmentReport() {
   };
 
   return (
-    <div className="p-6 bg-[#FFFAFA] h-[55vh] overflow-y-scroll">
+    <div className="p-6 bg-[#FFFAFA] h-[55vh] ">
       <h1 className="text-3xl font-extrabold mb-6 text-[#1b5b40]">
         Rabies Vaccination Accomplishment Report
       </h1>
