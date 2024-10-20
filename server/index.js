77d1a7be198f3d8b41a91ac60b49d86947507b91
Abakus <yacapinton@gmail.com>
2024-10-20 23:05:28 +0800

    updated Server

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ const user = require('./routes/userRoutes');
 const auth = require('./routes/loginRoute');
 const animalhealthcareservicesRoutes = require('./routes/Client/animalhealthcareservicesRoutes')
 const animalProductionServicesRoutes = require('./routes/Client/animalproductionservicesRoutes'); 
+const veterinaryInformationServiceRoutes = require("./routes/Client/veterinaryinformationserviceRoutes");
 
 // Import the audit log middleware
 const auditLogMiddleware = require('./middleware/auditlogMiddleware');
@@ -68,6 +69,8 @@ app.use('/', RoutineServicesMonitoringReport);
 app.use('/', RabiesHistoryRoutes);
 app.use('/api/animal-health-care-services', animalhealthcareservicesRoutes);
 app.use('/api/animal-production-services', animalProductionServicesRoutes);
+app.use("/api/veterinary-information-service", veterinaryInformationServiceRoutes);
+
 
 
 app.use('/api', auditLogRoutes);
