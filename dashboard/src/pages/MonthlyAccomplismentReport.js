3411b0b1c9d1414b5844ced745a602403a8b3534
Trent <65882951+Dre-OS@users.noreply.github.com>
2024-10-29 15:43:47 +0800

    remove button when not admin

--- dashboard/src/pages/MonthlyAccomplismentReport.js
+++ dashboard/src/pages/MonthlyAccomplismentReport.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AccomplishmentReport from "./AccomplishmentReport";
 import RSMAccomplishmentReport from "./RSMAccomplishmentReport";
 import RabiesVaccinationAccomplishmentReport from "./RabiesVaccinationAccomplishmentReport";
@@ -10,10 +10,26 @@ import MunicipalityAccomplishmentReportVaccination from "./MunicipalityAccomplis
 import MunicipalityAccomplishmentReportRabies from "./MunicipalityAccomplishmentReportRabies";
 import MunicipalityAccomplishmentReportRoutineServices from "./MunicipalityAccomplishmentReportRoutineServices";
 import CardBox from "../component/CardBox";
+import { jwtDecode } from "jwt-decode";
 
 function MonthlyAccomplishmentReport() {
   const [isModalOpen, setModalOpen] = useState(false);
   const [currentStep, setCurrentStep] = useState(0);
+  const [userRole, setUserRole] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        const role = decodedToken.role;
+        console.log(role); // Adjust this key based on your token's structure
+        setUserRole(role);
+      } catch (error) {
+        console.error("Invalid token", error);
+      }
+    }
+  }, []);
 
   const pages = [
     {
@@ -66,26 +82,29 @@ function MonthlyAccomplishmentReport() {
         <div className="p-6 space-y-8 bg- min-h-0">
           {/* Button to open the modal */}
 
-            <div className="flex flex-row mb-4 justify-between">
-              <h2 className="text-3xl font-extrabold mb-6 text-darkgreen">
-                Monthly Accomplishment Report
-              </h2>
+          <div className="flex flex-row mb-4 justify-between">
+            <h2 className="text-3xl font-extrabold mb-6 text-darkgreen">
+              Monthly Accomplishment Report
+            </h2>
+            {userRole === "admin" && (
               <button
                 className="px-4 py-2 bg-darkgreen hover:bg-darkergreen text-white rounded"
                 onClick={handleOpenModal}
               >
-                {currentStep >= 3 ? "Open Target Form Per Municipality" : "Open Provicial Target Form" }
+                {currentStep >= 3
+                  ? "Open Target Form Per Municipality"
+                  : "Open Provincial Target Form"}
               </button>
-              
-            </div>
-            {/* Render the StepperComponent */}
-            <CardBox>
+            )}
+          </div>
+          {/* Render the StepperComponent */}
+          <CardBox>
             <StepperComponent
               pages={pages}
               renderStepContent={renderStepContent}
               onStepChange={setCurrentStep}
             />
-            </CardBox>
+          </CardBox>
           {/* Modal for TargetForm */}
           <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
             {currentStep >= 3 ? <MunicipalityTargetList /> : <TargetList />}
@@ -95,8 +114,12 @@ function MonthlyAccomplishmentReport() {
       {/* This will display the message for screens smaller than medium (md) */}
       <div className="block lg:hidden">
         <div className="p-6 bg-green-100 text-green-800 text-center rounded-lg">
-          <h2 className="text-xl font-semibold">Mobile does not support this feature</h2>
-          <p>Please visit our desktop site to access this feature. Thank you!</p>
+          <h2 className="text-xl font-semibold">
+            Mobile does not support this feature
+          </h2>
+          <p>
+            Please visit our desktop site to access this feature. Thank you!
+          </p>
         </div>
       </div>
     </>
